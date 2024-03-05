bd1c25b920b88f3ba15a060e8c49c58645618e29
Calin Anca <calinanca99@gmail.com>
2024-03-05 15:25:45 +0100

    Read file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::io::Read;
+
 fn main() {
-    println!("Hello, world!");
+    let args = std::env::args().collect::<Vec<_>>();
+    let file_path = if let Some(path) = args.get(1) {
+        path
+    } else {
+        eprintln!("USAGE: rs86 <FILE_PATH>");
+        return;
+    };
+
+    let mut file = std::fs::File::open(file_path).expect("Cannot open file");
+    let mut buf = [0_u8; 2];
+    file.read_exact(&mut buf).expect("Cannot read from file");
+
+    println!("Buf: {:?}", buf);
 }
