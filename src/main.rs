9b6ec30630406e2df07b507639fed11137f19e48
Calin Anca <calinanca99@gmail.com>
2024-03-05 16:00:16 +0100

    Check the opcode

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::io::Read;
 
+const MOV_OPCODE: u8 = 0b0010_0010;
+
 fn main() {
     let args = std::env::args().collect::<Vec<_>>();
     let file_path = if let Some(path) = args.get(1) {
@@ -12,6 +14,17 @@ fn main() {
     let mut file = std::fs::File::open(file_path).expect("Cannot open file");
     let mut buf = [0_u8; 2];
     file.read_exact(&mut buf).expect("Cannot read from file");
-
     println!("Buf: {:?}", buf);
+
+    // -> Check the instruction code
+    // Ignore D, W flags (the least significant two bits) from the first
+    // byte by shifting right two bits.
+    println!("First byte: {:#b}", buf[0]);
+    let opcode = buf[0] >> 2;
+    println!("Instruction code: {:#010b}", opcode);
+
+    if opcode != MOV_OPCODE {
+        eprintln!("OPCODE not supported");
+        return;
+    }
 }
