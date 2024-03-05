6b0b5214be7cc630afe609faae3d913ac753edb6
Calin Anca <calinanca99@gmail.com>
2024-03-05 18:11:44 +0100

    Completely parse the MOV instruction

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,61 @@ use std::io::Read;
 
 const MOV_OPCODE: u8 = 0b0010_0010;
 
+#[derive(Debug)]
+enum Register {
+    AL,
+    CL,
+    DL,
+    BL,
+    AH,
+    CH,
+    DH,
+    BH,
+    AX,
+    CX,
+    DX,
+    BX,
+    SP,
+    BP,
+    SI,
+    DI,
+}
+
+impl Register {
+    fn new(value: u8, is_word: bool) -> Result<Register, String> {
+        if value > 7 {
+            return Err("Value cannot be higher than 7".to_string());
+        }
+
+        let r = if is_word {
+            match value {
+                0 => Register::AX,
+                1 => Register::CX,
+                2 => Register::DX,
+                3 => Register::BX,
+                4 => Register::SP,
+                5 => Register::BP,
+                6 => Register::SI,
+                7 => Register::DI,
+                _ => unreachable!(),
+            }
+        } else {
+            match value {
+                0 => Register::AL,
+                1 => Register::CL,
+                2 => Register::DL,
+                3 => Register::BL,
+                4 => Register::AH,
+                5 => Register::CH,
+                6 => Register::DH,
+                7 => Register::BH,
+                _ => unreachable!(),
+            }
+        };
+        Ok(r)
+    }
+}
+
 fn main() {
     let args = std::env::args().collect::<Vec<_>>();
     let file_path = if let Some(path) = args.get(1) {
@@ -16,15 +71,35 @@ fn main() {
     file.read_exact(&mut buf).expect("Cannot read from file");
     println!("Buf: {:?}", buf);
 
-    // -> Check the instruction code
-    // Ignore D, W flags (the least significant two bits) from the first
-    // byte by shifting right two bits.
     println!("First byte: {:#b}", buf[0]);
+    println!("Second byte: {:#b}", buf[1]);
     let opcode = buf[0] >> 2;
-    println!("Instruction code: {:#010b}", opcode);
 
     if opcode != MOV_OPCODE {
         eprintln!("OPCODE not supported");
         return;
     }
+
+    if !(buf[1] >> 6) == 3 {
+        eprintln!("MOD not supported");
+        return;
+    };
+
+    let operates_on_word = (buf[0] & 0b0000_0001) == 1;
+    let destination_in_reg = (buf[0] & 0b0000_0010) >> 1 == 1;
+
+    let reg = (buf[1] & 0b0011_1000) >> 3;
+    let rm = buf[1] & 0b0000_0111;
+
+    let reg_register = Register::new(reg, operates_on_word).unwrap();
+    let rm_register = Register::new(rm, operates_on_word).unwrap();
+
+    let (source, destination) = if destination_in_reg {
+        (rm_register, reg_register)
+    } else {
+        (reg_register, rm_register)
+    };
+
+    dbg!(&source);
+    dbg!(&destination);
 }
